9c302f5bffa4014ae53e49be36fb006aa04b634c
ngrhadi <adinugraha.ista@yahoo.com>
2021-06-30 22:35:37 +0700

    first commit

---
+++ html/data/LocationProject_2.js
@@ -0,0 +1,25 @@
+var json_LocationProject_2 = {
+  type: 'FeatureCollection',
+  name: 'LocationProject_2',
+  crs: { type: 'name', properties: { name: 'urn:ogc:def:crs:OGC:1.3:CRS84' } },
+  features: [
+    { type: 'Feature', properties: { Name: 'Lawe Alas', Regency: 'Subulussalam', Province: 'Aceh Tenggara', Category: 'Hidroelectrical Power Project' }, geometry: { type: 'Point', coordinates: [97.948259631876624, 2.93059339702541] } },
+    { type: 'Feature', properties: { Name: 'Grindulu Fault', Regency: 'Pacitan', Province: 'Jawa Timur', Category: 'Morpho-Tectonic Research Study' }, geometry: { type: 'Point', coordinates: [111.204284723786444, -8.068188989204923] } },
+    {
+      type: 'Feature',
+      properties: { Name: 'Sangkuliran Jetty Acces', Regency: 'Kutai Timur', Province: 'Kalimantan Timur', Category: 'Road-Drainase Improvment' },
+      geometry: { type: 'Point', coordinates: [117.929532340139858, 1.178759353966722] },
+    },
+    { type: 'Feature', properties: { Name: 'Lore Lindu', Regency: 'Sigi', Province: 'Sulawesi Tengah', Category: 'Hidroelectrical Power Project' }, geometry: { type: 'Point', coordinates: [120.107170510163428, -1.227541509081873] } },
+    {
+      type: 'Feature',
+      properties: { Name: 'Ende-Kelimutu Landslide', Regency: 'Ende', Province: 'Nusa Tenggara Timur', Category: 'Landsllide Detail Analysis Project' },
+      geometry: { type: 'Point', coordinates: [121.780468534823299, -8.706093791709367] },
+    },
+    {
+      type: 'Feature',
+      properties: { Name: 'Tanimbar Water Resources', Regency: 'Tanimbar', Province: 'Maluku Barat', Category: 'Water Resources Management Project' },
+      geometry: { type: 'Point', coordinates: [131.290539252562326, -7.885472538236316] },
+    },
+  ],
+};
